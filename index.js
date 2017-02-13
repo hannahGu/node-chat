a20c96475bab8096b974c5d1dadcbe016630f2ae
hannahgu <hannahgu@vocinno.com>
2017-02-13 21:22:51 +0800

    add more clients

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-
-var app = require('express')();
+var express = require("express");
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 server.listen(8000);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
 // });
 
+app.use('/',express.static(__dirname +'/'));
 
 
+var list = [];
 io.on("connection",function(socket){
   socket.on("news",function(data){
+    list.push(data);
     console.log(data);
-    socket.emit("other event",{oo:"other"});
+    socket.emit("toClient",list);
   });
 });
